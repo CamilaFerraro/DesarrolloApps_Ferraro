dadb679a8639f8ff4b4aca821ba27e215d435fc7
CamiFerraro <camiferraroo@gmail.com>
2022-11-20 20:44:17 -0300

    navigation

--- src/screens/BreadDetailsScreen.js
+++ src/screens/BreadDetailsScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 
-const BreadDetailsScreen = () => {
+const BreadDetailsScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bread Category</Text>
+      <Button title="Go back to Categories" onPress={() => navigation.navigate("Categories")} />
     </View>
   )
 }
