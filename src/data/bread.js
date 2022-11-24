7dd5737f4d2bf093096267ee5f19a0d9f7546d5f
CamiFerraro <camiferraroo@gmail.com>
2022-11-23 22:24:39 -0300

    componentes y navegacion

---
+++ src/data/bread.js
@@ -0,0 +1,34 @@
+export const BREADS = [
+    {
+        id: "1",
+        category: "1",
+        name: "Pan Baguette",
+        description: "Clasico pan frances",
+        weight: "350gr",
+        price: 16,
+    },
+    {
+        id: "2",
+        category: "2",
+        name: "Pan Cibatta Blanco",
+        description: "Pan de alta hidratacion",
+        weight: "800gr",
+        price: 30,
+    },
+    {
+        id: "3",
+        category: "3",
+        name: "Pan de Hamburguesa",
+        description: "Textura suave, decorado con semillas de sesamo",
+        weight: "120gr",
+        price: 5,
+    },
+    {
+        id: "4",
+        category: "4",
+        name: "Pan de Molde Integral de Centeno",
+        description: "Pidelo entero o tajado, con o sin semillas",
+        weight: "900gr",
+        price: 35,
+    },
+];
---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+    {
+        id: "1",
+        title: "Harinas Puras",
+        color: "#896978",
+    },
+    {
+        id: "2",
+        title: "Harinas Integrale",
+        color: "#839791",
+    },
+    {
+        id: "1",
+        title: "Panes Saborizados",
+        color: "#aac0af",
+    },
+    {
+        id: "1",
+        title: "Otros Productos de Panaderia",
+        color: "#896978",
+    },
+]
